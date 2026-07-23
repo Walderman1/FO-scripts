117e888a1c77ca1a7ec95449d9daa4bfca3c6f55
Walderman1 <76510884+Walderman1@users.noreply.github.com>
2026-07-23 21:00:21 +0500

    Update DialogueTrigger.cs

--- Scripts/Dialogue/DialogueTrigger.cs
+++ Scripts/Dialogue/DialogueTrigger.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public int dialogueFileIndex = 1; // Индекс файла Trixie.txt
+    public int dialogueFileIndex = 1;
 
-    // Этот скрипт просто маркер для определения клика
+    private void Awake()
+    {
+        Logger.Log(LogModule.Dialogue, $"DialogueTrigger инициализирован на {gameObject.name}, индекс файла: {dialogueFileIndex}");
+    }
+
+    private void OnDestroy()
+    {
+        Logger.Log(LogModule.Dialogue, $"DialogueTrigger уничтожен на {gameObject.name}");
+    }
 }
